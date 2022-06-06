ec49819fd8046212235304e6e25cd5cd3bd0d51c
nanikon <nata.nik.18@mail.ru>
2022-06-06 07:46:37 +0300

    add equations

--- differential/src/main/kotlin/model/DiffEquation.kt
+++ differential/src/main/kotlin/model/DiffEquation.kt
@@ -2,7 +2,9 @@ package model
 
 import java.math.BigDecimal
 import java.math.MathContext
+import kotlin.math.cos
 import kotlin.math.exp
+import kotlin.math.sin
 
 /**
  * @author Natalia Nikonova
@@ -38,5 +40,19 @@ val equations = listOf(
         },
         "y' = y + (1 + x) * y^2",
         { c -> "y = -e^x / (x * e^x + $c)" }
+    ),
+     DiffEquation(
+         { x, y -> (x - y).pow(2) + BigDecimal.ONE },
+         { x0, y0 -> BigDecimal.ONE.divide(x0 - y0, MathContext.DECIMAL32) - x0 },
+         { x, c -> x - BigDecimal.ONE.divide(x + c, MathContext.DECIMAL32) },
+         "y' = (x - y)^2 + 1",
+         { c -> "y = x - 1 / (x + $c)" }
+     ),
+    DiffEquation(
+        { x, y -> y * sin(x.toDouble()).toBigDecimal(MathContext.DECIMAL32) },
+        { x0, y0 -> y0 * exp(cos(x0.toDouble())).toBigDecimal(MathContext.DECIMAL32)},
+        { x, c -> c.divide(exp(cos(x.toDouble())).toBigDecimal(MathContext.DECIMAL32), MathContext.DECIMAL32) },
+        "y' = y * sin(x)",
+        { c -> "$c / e^(cos(x))"}
     )
 )
