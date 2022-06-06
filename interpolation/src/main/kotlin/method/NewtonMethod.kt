dae227012c45768ee91b32a34725686967d614ca
nanikon <nata.nik.18@mail.ru>
2022-06-06 14:47:42 +0300

    correct output

--- interpolation/src/main/kotlin/method/NewtonMethod.kt
+++ interpolation/src/main/kotlin/method/NewtonMethod.kt
@@ -25,35 +25,42 @@ class NewtonMethod : Method {
         val middle = (xs[0] + xs.last()).divide(BigDecimal(2), MathContext.DECIMAL32)
         val h = xs[1] - xs[0]
         var y = BigDecimal.ZERO
-        print("Значение многочлена Ньютона при x=$x: ")
+        // print("Значение многочлена Ньютона при x=$x: ")
+        var stringEq = ""
+        var stringDiff = ""
+        // print("Используемые конечные разности: ")
         if (x < middle) {
             // формула вперед
-            print("!")
+            println("Используется формула вперед")
             val a = xs.maxOf { if (it < x) it else xs[0] }
             val t = (x - a).divide(h, MathContext.DECIMAL32)
             val i = xs.indexOf(a)
             var factor = BigDecimal.ONE
             for (j in finiteDifferences[i].indices) {
-                if (j != 0) { print(" + ") }
-                print("${finiteDifferences[j][i]} * $factor")
+                if (j != 0) { stringEq += " + " }
+                stringEq += "${finiteDifferences[j][i]} * $factor"
                 y += finiteDifferences[j][i] * factor
+                stringDiff += "${finiteDifferences[j][j]} "
                 factor *= (t - j.toBigDecimal()).divide((j + 1).toBigDecimal(), MathContext.DECIMAL32)
             }
         } else {
             // формула назад
+            println("Используется формула назад")
             val a = xs.minOf { if (it > x) it else xs.last() }
             val t = (x - a).divide(h, MathContext.DECIMAL32)
             val i = xs.indexOf(a)
             var factor = BigDecimal.ONE
             for (j in i downTo 0) {
-                if (j != i) { print(" + ") }
+                if (j != i) { stringEq += " + " }
                 val c = i - j
-                print("${finiteDifferences[c][j]} * $factor")
+                stringEq += "${finiteDifferences[c][j]} * $factor"
                 y += finiteDifferences[c][j] * factor
+                stringDiff += "${finiteDifferences[c][j]} "
                 factor *= (t + c.toBigDecimal()).divide((c + 1).toBigDecimal(), MathContext.DECIMAL32)
             }
         }
-        println(" = $y")
+        println("Используемые конечные разности $stringDiff")
+        println("\nЗначение многочлена Ньютона при x=$x: $stringEq = $y")
         return Pair(x, y)
     }
 }
