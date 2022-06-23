8c73444640238a90e2dc82638e310fd16b8cb436
nanikon <nata.nik.18@mail.ru>
2022-06-24 02:18:36 +0300

    add report

--- differential/src/main/kotlin/method/AdamsMethod.kt
+++ differential/src/main/kotlin/method/AdamsMethod.kt
@@ -23,12 +23,6 @@ object AdamsMethod {
                             BigDecimal(37) * diff.function(xs[i - 3], ys[i - 3]) -
                             BigDecimal(9) * diff.function(xs[i - 4], ys[i - 4])
                     )
-            /*println("$newY = ${ys[i - 1]} +\n" +
-                    "                    ${h.divideN(24)}3 * (\n" +
-                    "                    BigDecimal(55) * ${diff.function(xs[i - 1], ys[i - 1]) }-\n" +
-                    "                            BigDecimal(59) * ${diff.function(xs[i - 2], ys[i - 2])} +\n" +
-                    "                            BigDecimal(37) * ${diff.function(xs[i - 3], ys[i - 3])} -\n" +
-                    "                            BigDecimal(9) * ${diff.function(xs[i - 4], ys[i - 4])}")*/
             var oldY = newY
             var count = 0
             do {
@@ -40,14 +34,12 @@ object AdamsMethod {
                                 BigDecimal(5) * diff.function(xs[i - 2], ys[i - 2]) +
                                 BigDecimal(1) * diff.function(xs[i - 3], ys[i - 3])
                         )
-                // println("$i-й y: $oldY, $newY, ${oldY - newY}, $count")
                 count++
                 if (count == 100) {
                     println("Корректирующие значения расходятся. Нужная точность не достигается")
                     break
                 }
             } while ((oldY - newY).abs() >= eps)
-            println("$i-ый y - $count")
             ys.add(newY)
         }
         return ys
